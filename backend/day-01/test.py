5188b8553a0428beb2fe13e63e64705248b5df66
Geru <113689254+Gelatino0308@users.noreply.github.com>
2023-11-20 19:30:01 +0800

    Updated test.py

--- backend/day-01/test.py
+++ backend/day-01/test.py
@@ -1,6 +1,6 @@
 name = "Angelo"
 age = 20
 isMale = True
-department = "Backend Development Team"
+department = "DSWD"
 
 print(f"{name} is a {age}-year old {'male' if isMale else 'female'} participating in {department}'s 30 Days of Coding.")

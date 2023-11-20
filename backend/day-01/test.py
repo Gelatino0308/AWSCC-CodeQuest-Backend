5e989e56140aa9789fc9eb3955a8cefb04dd83d1
Geru <113689254+Gelatino0308@users.noreply.github.com>
2023-11-20 19:18:25 +0800

    Update test.py

--- backend/day-01/test.py
+++ backend/day-01/test.py
@@ -1,6 +1,6 @@
-name = None
-age = None
-isMale = None
-department = None
+name = "Angelo"
+age = 20
+isMale = True
+department = "Backend Development Team"
 
-print(f"{name} is an {age}-year old {'male' if isMale else 'female'} participating in {department}'s 30 Days of Coding.")
+print(f"{name} is a {age}-year old {'male' if isMale else 'female'} participating in {department}'s 30 Days of Coding.")

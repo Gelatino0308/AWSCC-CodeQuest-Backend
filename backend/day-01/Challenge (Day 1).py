729da09511d992bb595c9fc47381b582bc80caaa
Geru <113689254+Gelatino0308@users.noreply.github.com>
2023-11-20 19:30:52 +0800

    Created Challenge (Day 1).py

---
+++ backend/day-01/Challenge (Day 1).py
@@ -0,0 +1,9 @@
+#Hello, I am {your_name}. I love {name_of_department} and I love being in the Backend even more!
+#One thing about me is that {about_you}.
+
+your_name = "Angelo"
+name_of_department = "DSWD"
+about_you = "I love webtoons"
+
+print(f"Hello, I am {your_name}. I love {name_of_department} and I love being in the Backend even more!")
+print(f"One thing about me is that {about_you}.")

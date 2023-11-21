a6f9af3aec2bd8c41631faa25b6e7243cb434841
Geru <113689254+Gelatino0308@users.noreply.github.com>
2023-11-21 11:04:32 +0800

    Created Challenge (Day 2).py

---
+++ submissions/Challenge Time Codes/Challenge (Day 2).py
@@ -0,0 +1,12 @@
+#Enter the first number: 5
+#Enter the second number: 19
+#Enter the third number: 11
+
+#Sum: 35
+
+first_num = int(input("Enter the first number: "))
+second_num = int(input("Enter the second number: "))
+third_num = int(input("Enter the third number: "))
+
+sum = first_num + second_num + third_num
+print(f"\nSum: {sum}")

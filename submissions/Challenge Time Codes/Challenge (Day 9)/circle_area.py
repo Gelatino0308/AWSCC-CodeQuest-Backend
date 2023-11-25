2abacfdb59bda3c80ebc41ffa01c9e756a8be19e
Geru <113689254+Gelatino0308@users.noreply.github.com>
2023-11-25 11:54:17 +0800

    Created Challenge (Day 9) folder

---
+++ submissions/Challenge Time Codes/Challenge (Day 9)/circle_area.py
@@ -0,0 +1,4 @@
+import math
+
+def calculate_area (r):
+    return math.pi * r**2
---
+++ submissions/Challenge Time Codes/Challenge (Day 9)/main.py
@@ -0,0 +1,6 @@
+from circle_area import calculate_area
+
+radius = 5
+area_of_circle = calculate_area(radius)
+print(f"The area of a circle with a radius of {radius} is", area_of_circle)
+
